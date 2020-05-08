f79ce07b5372d24ac2706a73dc708b498a2ce871
sintakaridina <sintakaridina@gmail.com>
2020-05-08 15:50:56 +0700

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,10 +15,10 @@
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   <!-- CSS Files -->
-  <link href="{{URL::asset('css/bootstrap.min.css')}}" rel="stylesheet" />
-  <link href="{{URL::asset('css/now-ui-kit.css?v=1.3.0')}}" rel="stylesheet" />
+  <link href="css/bootstrap.min.css" rel="stylesheet" />
+  <link href="css/now-ui-kit.css?v=1.3.0" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="{{URL::asset('demo/demo.css')}}" rel="stylesheet" />
+  <link href="demo/demo.css" rel="stylesheet" />
 </head>
 
 
